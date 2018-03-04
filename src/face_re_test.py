762823453679d9a7f48db89c6fdd5c2239aaa424
venidi <venidi@163.com>
2018-03-04 15:06:13 +0800

    add comments

---
+++ src/face_re_test.py
@@ -0,0 +1,89 @@
+# import cv2
+# import numpy as np
+#
+#
+# def show():
+#     cap = cv2.VideoCapture(r'/home/venidi/FaceRecognition/data/videos/kbj.mp4')
+#     timeF = 3
+#     c = 1
+#     while(1):
+#         # get a frame
+#
+#         ret, frame = cap.read()
+#         if c % timeF == 0:
+#             # show a frame
+#             cv2.imshow("capture", frame)
+#         c = c + 1
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     cap.release()
+#     cv2.destroyAllWindows()
+#
+# if __name__ == '__main__':
+#     show()
+
+# -*- coding: utf-8 -*-
+
+
+# 样本视频处理，获取视频中人脸作为识别样本
+
+import cv2 as cv
+
+
+def face_cut(video_path,save_path):
+    cv.namedWindow('PeopleRecognition', cv.WINDOW_NORMAL)
+    # video = cv.VideoCapture(r'/home/venidi/FaceRecognition/data/videos/kbjdkr.mp4')
+    video = cv.VideoCapture(video_path)
+    #path_name = r'./faces/faceO'
+    path_name = save_path
+    # openCV的人脸分类器
+    classifier = cv.CascadeClassifier(r'/opt/opencv34/data/haarcascades/haarcascade_frontalface_alt.xml')
+
+    color = (0, 135, 255)
+    num = 0
+
+    TIME = 3
+    C = 1
+    # 逐帧检测
+    while (1):
+        ok,frame = video.read()
+        if not ok:
+            break
+        # 转换成灰度图像便于计算
+        if C % TIME == 0:
+            gery = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            # 人脸检测，1.2和2分别为图片缩放比例和需要检测的有效点数
+            faceRects = classifier.detectMultiScale(gery, scaleFactor=1.2, minNeighbors=2, minSize=(32,32))
+            if len(faceRects) > 0:
+                for faceRect in faceRects:
+                    x, y, w, h = faceRect
+                    # 截取保存
+                    img_name = '%s/%d.jpg' % (path_name, num)
+                    # print(img_name)
+                    image = frame[y-10: y+h+10, x-10: x+w+10]
+                    cv.imwrite(img_name,image)
+                    num += 1
+                    if num > 1000:
+                        break
+                    # # 框出人脸
+                    cv.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), color, 2)
+
+                    # 显示当前照片编号
+                    font = cv.FONT_HERSHEY_SIMPLEX
+                    cv.putText(frame, 'num:%d' % (num), (x + 30, y + 30), font, 1, (255, 0, 255), 4)
+        C = C + 1
+        # 超过指定最大保存数量结束程序
+        if num > 1000:
+            break
+
+        cv.imshow('PeopleRecognition', frame)
+        c = cv.waitKey(1)
+        if c & 0xFF == ord('q'):
+            break
+    video.release()
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    video_path = r'/home/venidi/FaceRecognition/data/videos/kbjdkr.mp4'
+    save_path = r'/home/venidi/FaceRecognition/test/LinuxProject/faces/face_o'
+    face_cut(video_path, save_path)
--- src/face_recognitation.py
+++ src/face_recognitation.py
@@ -9,34 +9,39 @@ if __name__ == '__main__':
 
     color = (0, 255, 0)
     cv.namedWindow('PeopleRecognition', 0)
-    video = cv.VideoCapture(r'/home/venidi/FaceRecognition/data/videos/kbjdkr.mp4')
+    video = cv.VideoCapture(r'/home/venidi/FaceRecognition/data/videos/kbj.mp4')
 
     classifier = cv.CascadeClassifier(r'/opt/opencv34/data/haarcascades/haarcascade_frontalface_alt2.xml')
 
+    TIME = 3
+    C = 1
     while video.isOpened():
         ok,frame = video.read()
         if not ok:
             print('not ok')
             break
-        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        faceRects = classifier.detectMultiScale(frame_gray, scaleFactor=1.2, minNeighbors=3, minSize=(32,32))
-        if len(faceRects) > 0:
-            for faceRect in faceRects:
-                x, y, w, h = faceRect
+        if C % TIME == 0:
+            frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            faceRects = classifier.detectMultiScale(frame_gray, scaleFactor=1.2, minNeighbors=3, minSize=(32,32))
+            if len(faceRects) > 0:
+                for faceRect in faceRects:
+                    x, y, w, h = faceRect
 
-                image = frame[y-10: y+h+10, x-10: x+w+10]
-                faceID = model.face_predict(image)
-                # 识别并显示
-                if faceID == 0:
-                    cv.rectangle(frame, (x-10, y-10), (x+w+10, y+h+10), color, thickness=2)
-                    cv.putText(frame, 'FACE_D', (x+30, y+30), cv.FONT_HERSHEY_SIMPLEX, 1,(255, 28, 33), 2)
-                else:
-                    # pass
-                    cv.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), color, thickness=2)
-        cv.imshow('result', frame)
-        key = cv.waitKey(10)
-        if key & 0xFF == ord('q'):
-            break
+                    image = frame[y-10: y+h+10, x-10: x+w+10]
+                    faceID = model.face_predict(image)
+                    # 识别并显示
+                    if faceID == 0:
+                        cv.rectangle(frame, (x-10, y-10), (x+w+10, y+h+10), color, thickness=2)
+                        cv.putText(frame, 'FACE_D', (x+30, y+30), cv.FONT_HERSHEY_SIMPLEX, 1,(255, 28, 33), 2)
+                    else:
+                        # pass
+                        cv.rectangle(frame, (x - 10, y - 10), (x + w + 10, y + h + 10), color, thickness=2)
+            cv.imshow('result', frame)
+            key = cv.waitKey(1)
+
+            if key & 0xFF == ord('q'):
+                break
+        C = C + 1
 
     video.release()
     cv.destroyAllWindows()
